b0a411f01940f6ae2323beb4b89677ccbe96a4de
Franco Avellaneda <francoavellaneda773@gmail.com>
2026-08-03 19:42:37 -0300

    Update MostrarEstado.cs

--- MostrarEstado.cs
+++ MostrarEstado.cs
@@ -6,7 +6,7 @@ public partial class Programa{
 	static void MostrarEstado(){
        Console.WriteLine("Estados de postulantes:");
 
-        for (int i=0; i<cantEstudiantes; i++){
+        for (int i=0;i<cantEstudiantes;i++){
         Postulantes[i].Estado = ObtenerEstado(Postulantes[i]);
 
     Console.WriteLine(Postulantes[i].Legajo + " - " + Postulantes[i].Nombre + " " + Postulantes[i].Apellido + " - Estado: " + Postulantes[i].Estado);
